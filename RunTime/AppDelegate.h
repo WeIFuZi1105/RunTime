684409bdcb21accbf9ac67ee74f391608c0f6ef9
WeIFuZi1105 <yuyekuangmo@qq.com>
2016-07-06 10:56:06 +0800

    Initial Commit

---
+++ RunTime/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RunTime
+//
+//  Created by 三才汇隆 on 16/6/27.
+//  Copyright © 2016年 alai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RunTime/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RunTime
+//
+//  Created by 三才汇隆 on 16/6/27.
+//  Copyright © 2016年 alai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
